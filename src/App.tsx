add47aaa6292fdcbe0b2d96da13a1d303b701069
mxdmrt <34488580+mxdmrt@users.noreply.github.com>
2023-07-30 17:07:40 +0200

    Replace `App.css` with Emotion components

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,108 @@
-import "./App.css";
+import styled from "@emotion/styled";
 import emotionLogo from "./assets/emotion.png";
 import reactLogo from "./assets/react.svg";
 import zustandLogo from "./assets/zustand.png";
 import { useStore } from "./store/Store";
 import viteLogo from "/vite.svg";
 
+const StyledMain = styled.main`
+  display: grid;
+  row-gap: 4rem;
+  justify-items: center;
+`;
+
+const StyledLogosWrapper = styled.div`
+  display: inline-grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 2rem;
+
+  @media (min-width: 670px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
+
+const StyledLogo = styled.a`
+  @keyframes logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  display: inline-flex;
+
+  & img {
+    display: block;
+    height: 6rem;
+    will-change: filter;
+    transition: filter 300ms;
+
+    &:hover {
+      filter: drop-shadow(0 0 2rem #646cffaa);
+    }
+
+    &.react {
+      &:hover {
+        filter: drop-shadow(0 0 2rem #61dafbaa);
+      }
+
+      @media (prefers-reduced-motion: no-preference) {
+        & {
+          animation: logo-spin infinite 20s linear;
+        }
+      }
+    }
+
+    &.zustand:hover {
+      filter: drop-shadow(0 0 2rem #f56d2caa);
+    }
+
+    &.emotion:hover {
+      filter: drop-shadow(0 0 2rem #d36ac2aa);
+    }
+  }
+`;
+
+const StyledCard = styled.div`
+  display: grid;
+  row-gap: 1rem;
+  justify-items: center;
+`;
+
+const StyledParagraph = styled.p`
+  color: #888;
+`;
+
 function App() {
   const { count, setCount } = useStore();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://zustand-demo.pmnd.rs/" target="_blank">
-          <img src={zustandLogo} className="logo zustand" alt="Zustand logo" />
-        </a>
-        <a href="https://emotion.sh" target="_blank">
-          <img src={emotionLogo} className="logo emotion" alt="Emotion logo" />
-        </a>
-      </div>
+    <StyledMain>
+      <StyledLogosWrapper>
+        <StyledLogo href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} alt="Vite logo" />
+        </StyledLogo>
+        <StyledLogo href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="react" alt="React logo" />
+        </StyledLogo>
+        <StyledLogo href="https://zustand-demo.pmnd.rs/" target="_blank">
+          <img src={zustandLogo} className="zustand" alt="Zustand logo" />
+        </StyledLogo>
+        <StyledLogo href="https://emotion.sh" target="_blank">
+          <img src={emotionLogo} className="emotion" alt="Emotion logo" />
+        </StyledLogo>
+      </StyledLogosWrapper>
       <h1>Vite + React + Zustand + Emotion</h1>
-      <div className="card">
+      <StyledCard>
         <button onClick={() => setCount(count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </StyledCard>
+      <StyledParagraph>Click on the logos to learn more</StyledParagraph>
+    </StyledMain>
   );
 }
 
